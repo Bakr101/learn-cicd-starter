a9fd232c36f9ff3e0381f4ee3896d7c5ccb7a655
Bakr101 <ahmed98bakr@gmail.com>
2025-01-15 11:11:57 +0200

    formatting test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -8,25 +8,25 @@ import (
 	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
 )
 
-func TestGetAPIKeys(t *testing.T){
-	tests := map[string]struct{
-		headers			http.Header
-		expectedKey		string
-		expectedError	error	
+func TestGetAPIKeys(t *testing.T) {
+	tests := map[string]struct {
+		headers       http.Header
+		expectedKey   string
+		expectedError error
 	}{
-		"NoAuthHeader" : {headers: http.Header{}, expectedKey: "", expectedError: auth.ErrNoAuthHeaderIncluded},
+		"NoAuthHeader":                 {headers: http.Header{}, expectedKey: "", expectedError: auth.ErrNoAuthHeaderIncluded},
 		"MalformedAuth-NoAPIKeyPrefix": {headers: http.Header{"Authorization": []string{"123Bakr"}}, expectedKey: "", expectedError: auth.ErrMalformedToken},
-		"MalformedAuth-MissingToken": {headers: http.Header{"Authorization": []string{"ApiKey"}}, expectedKey: "", expectedError: auth.ErrMalformedToken},
-		"ValidKey": {headers: http.Header{"Authorization": []string{"ApiKey bakr123"}}, expectedKey: "bakr123", expectedError: nil},
+		"MalformedAuth-MissingToken":   {headers: http.Header{"Authorization": []string{"ApiKey"}}, expectedKey: "", expectedError: auth.ErrMalformedToken},
+		"ValidKey":                     {headers: http.Header{"Authorization": []string{"ApiKey bakr123"}}, expectedKey: "bakr123", expectedError: nil},
 	}
 
-	for name, tc := range tests{
+	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
 			key, err := auth.GetAPIKey(tc.headers)
-			if key != tc.expectedKey{
+			if key != tc.expectedKey {
 				t.Errorf("expected API key:%v, got:%v", tc.expectedKey, key)
 			}
-			if  (err == nil && tc.expectedError != nil) || (err != nil && !errors.Is(err, tc.expectedError)){
+			if (err == nil && tc.expectedError != nil) || (err != nil && !errors.Is(err, tc.expectedError)) {
 				t.Errorf("expected error:%v, got:%v", tc.expectedError, err)
 			}
 		})
